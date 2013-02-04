f84544d3c1261089ddb0e90408323a122cf56964
oscarhou <oscargot@oscarbotnet.(none)>
2013-02-04 13:21:29 -0800

    Removed loop label from code so that it would compile.

--- sha3/sha3.c
+++ sha3/sha3.c
@@ -4,24 +4,28 @@
 #define LANE_WIDTH 4
 #define PERM_WIDTH 25 * LANE_WIDTH 
 // Simple loop with an array
-int roundFunction ()
+int roundFunction (int **A)
 {
     int C[ARRAY_WIDTH], D[ARRAY_WIDTH];
-    loop: for (int count = 0; count < ARRAY_WIDTH; count++)
+    for (int count = 0; count < ARRAY_WIDTH; count++)
     {
         C[count] = A[count][0] ^ A[count][1] ^ A[count][2] ^ A[count][3] ^ A[count][4];
     }
-    loop: for (int count = 0; count < ARRAY_WIDTH; count++)
+    for (int count = 0; count < ARRAY_WIDTH; count++)
     {
         C[count] = A[count][0] ^ A[count][1] ^ A[count][2] ^ A[count][3] ^ A[count][4];
     }
+    return 0;
     
 }
 int main() {
-    loop: for (int round = 0; round <1)
+    //Look into using loops for repeated instructions
+    int result = 0;
+    //loop: for (int round = 0; round <1; round++)
+    for (int round = 0; round <1; round++)
     {
     }
     int A[ARRAY_WIDTH][ARRAY_WIDTH], B[ARRAY_WIDTH][ARRAY_WIDTH];
     printf("Done!\n");
-    return sum;
+    return result;
 }
