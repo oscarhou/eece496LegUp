78f5f976d9bf4cfc9be68519f6790ddfe0b677f6
OscarHou <oscarhou@ubuntu.ubuntu-domain>
2013-02-20 01:33:40 -0800

    fixed rotation array issue where the code was not indexing the correct values in the table

--- sha3/sha3.c
+++ sha3/sha3.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 
 #define ARRAY_WIDTH 5
+#define B_SIZE 800
 #define ROUNDS 22 
-#define LANE_WIDTH 32
+#define LANE_BIT_SIZE 32
 #define PERM_WIDTH 25 * LANE_WIDTH 
 
 /*
 Round Constants
 */
-unsigned long long RC[24];
+long RC[24];
 /*
 Rotation Offset 2d Array
 */
-int rotateArray[5][5] = {0, 1, 62, 28, 27,
+long rotateArray[5][5] = {0, 1, 62, 28, 27,
                          36, 44, 6, 55, 20,
                          3, 10, 43, 25, 39,
                          41, 45, 15, 21, 8,
                          18, 2, 61, 56, 14};
 //Rotate a  LANE SIZE-bit integer
-int rotateLeft(int lane, int rotateCount)
+long rotateLeft(long lane, int rotateCount)
 {
-    return (lane << rotateCount) | (lane >> (LANE_WIDTH - rotateCount));
+    return (lane << rotateCount) | (lane >> (LANE_BIT_SIZE - rotateCount));
 }
 // Simple loop with an array
-void roundFunction (int A[ARRAY_WIDTH][ARRAY_WIDTH])
+void roundFunction (long A[ARRAY_WIDTH][ARRAY_WIDTH])
 {
     //Theta step
-    int C[ARRAY_WIDTH], D[ARRAY_WIDTH];
-    int B[5][5];
+    long C[ARRAY_WIDTH], D[ARRAY_WIDTH];
+    long B[5][5];
     //xor every lane within a column together
     //repeat this for all columns
     for (int x = 0; x < ARRAY_WIDTH; x++)
@@ -51,7 +52,8 @@ void roundFunction (int A[ARRAY_WIDTH][ARRAY_WIDTH])
     {
         for (int y = 0; y < ARRAY_WIDTH; y++)
         {
-            B[y][ (2*x+3*y) % 5] = rotateLeft(A[x][y], rotateArray[x][y]);
+            B[y][ (2*x+3*y) % 5] = rotateLeft(A[x][y], rotateArray[y][x]);
+        //    printf("Using Array Rotate value %d for x=%d, y =%d\n",rotateArray[y][x],x,y);
         }
     }
     
@@ -77,48 +79,48 @@ int main() {
     //     get distorted values. Will have to look into it and refactor if that is the case.
     //Look into using loops for repeated instructions
     int result = 0;
-    int A[ARRAY_WIDTH][ARRAY_WIDTH], B[ARRAY_WIDTH][ARRAY_WIDTH];
-    A[0][0] = 1;
-    A[0][1] = 2;
-    A[0][2] = 3;
-    A[0][3] = 4;
-    A[0][4] = 5;
-    A[1][0] = 2;
-    A[1][1] = 3;
-    A[1][2] = 4;
-    A[1][3] = 5;
-    A[1][4] = 6;
-    A[2][0] = 2;
-    A[2][1] = 4;
-    A[2][2] = 6;
-    A[2][3] = 7;
-    A[2][4] = 8;
-    A[3][4] = 1;
-    A[4][2] = 1;
-    RC[0] = 0x0000000000000001;
-    RC[1] = 0x0000000000008082;
-    RC[2] = 0x800000000000808A;
-    RC[3] = 0x8000000080008000;
-    RC[4] = 0x000000000000808B;
-    RC[5] = 0x0000000080000001;
-    RC[6] = 0x8000000080008081;
-    RC[7] = 0x8000000000008009;
-    RC[8] = 0x000000000000008A;
-    RC[9] = 0x0000000000000088;
-    RC[10] = 0x0000000080008009;
-    RC[11] = 0x000000008000000A;
-    RC[12] = 0x000000008000808B;
-    RC[13] = 0x800000000000008B;
-    RC[14] = 0x8000000000008089;
-    RC[15] = 0x8000000000008003;
-    RC[16] = 0x8000000000008002;
-    RC[17] = 0x8000000000000080;
-    RC[18] = 0x000000000000800A;
-    RC[19] = 0x800000008000000A;
-    RC[20] = 0x8000000080008081;
-    RC[21] = 0x8000000000008080;
-    RC[22] = 0x0000000080000001;
-    RC[23] = 0x8000000080008008;
+    long A[ARRAY_WIDTH][ARRAY_WIDTH], B[ARRAY_WIDTH][ARRAY_WIDTH];
+    A[0][0] = 0;
+    A[0][1] = 0;
+    A[0][2] = 0;
+    A[0][3] = 0;
+    A[0][4] = 0;
+    A[1][0] = 0;
+    A[1][1] = 0;
+    A[1][2] = 0;
+    A[1][3] = 0;
+    A[1][4] = 0;
+    A[2][0] = 0;
+    A[2][1] = 0;
+    A[2][2] = 0;
+    A[2][3] = 0;
+    A[2][4] = 0;
+    A[3][4] = 0;
+    A[4][2] = 0;
+    RC[0] = (long)0x0000000000000001;
+    RC[1] = (long)0x0000000000008082;
+    RC[2] = (long)0x800000000000808A;
+    RC[3] = (long)0x8000000080008000;
+    RC[4] = (long)0x000000000000808B;
+    RC[5] = (long)0x0000000080000001;
+    RC[6] = (long)0x8000000080008081;
+    RC[7] = (long)0x8000000000008009;
+    RC[8] = (long)0x000000000000008A;
+    RC[9] = (long)0x0000000000000088;
+    RC[10] = (long)0x0000000080008009;
+    RC[11] = (long)0x000000008000000A;
+    RC[12] = (long)0x000000008000808B;
+    RC[13] = (long)0x800000000000008B;
+    RC[14] = (long)0x8000000000008089;
+    RC[15] = (long)0x8000000000008003;
+    RC[16] = (long)0x8000000000008002;
+    RC[17] = (long)0x8000000000000080;
+    RC[18] = (long)0x000000000000800A;
+    RC[19] = (long)0x800000008000000A;
+    RC[20] = (long)0x8000000080008081;
+    RC[21] = (long)0x8000000000008080;
+    RC[22] = (long)0x0000000080000001;
+    RC[23] = (long)0x8000000080008008;
 
 
     loop: for (int round = 0; round < ROUNDS; round++)
