1e0225f42e451221609d2dda6675f9a3557a04be
OscarHou <oscarhou@ubuntu.ubuntu-domain>
2013-03-04 01:05:42 -0800

    Changing the SHA-3 code to work in 16-bits seems to do the trick. Could be because of the soft-core processor that LegUP generates

--- sha3/sha3.c
+++ sha3/sha3.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 #define ARRAY_WIDTH 5
-#define B_SIZE 1600 
-#define ROUNDS 24
-#define LANE_BIT_SIZE 64 
+#define B_SIZE 400 
+#define ROUNDS 20
+#define LANE_BIT_SIZE 16
 #define PERM_WIDTH 25 * LANE_WIDTH 
 
-typedef unsigned long long UINT64;
-typedef UINT64 Lane;
+typedef unsigned short UINT16;
+typedef UINT16 Lane;
 Lane inputOne[5][5]= {1, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0,
-                        8000000000000000, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0};
+                      0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0};
+#define ROL(a, offset) ((((Lane)a) << ((offset) % LANE_BIT_SIZE)) ^ (((Lane)a) >> (LANE_BIT_SIZE-((offset) % LANE_BIT_SIZE))))
 
 /*
 Round Constants
 */
-UINT64 RC[24];
+UINT16 RC[24];
 /*
 Rotation Offset 2d Array
 */
@@ -25,11 +26,11 @@ Lane rotateArray[25] = {0, 1, 62, 28, 27,
                          36, 44, 6, 55, 20,
                          3, 10, 43, 25, 39,
                          41, 45, 15, 21, 8,
-                         18, 2, 61, 56, 14};
+                         18, 2, 61, 56, 14}; 
 //Rotate a  LANE SIZE-bit integer
 Lane rotateLeft(Lane lane, int rotateCount)
 {
-    return (lane << (rotateCount % LANE_BIT_SIZE)) ^ (lane >> (LANE_BIT_SIZE - (rotateCount % LANE_BIT_SIZE))) ; 
+    return ((Lane)lane << (rotateCount % LANE_BIT_SIZE)) ^ ((Lane)lane >> (LANE_BIT_SIZE - (rotateCount % LANE_BIT_SIZE))) ; 
 }
 
 int getIndex(unsigned int x, unsigned int y)
@@ -41,7 +42,7 @@ void roundFunction (Lane *A)
 {
     //Theta step
     unsigned int x,y;
-    UINT64 C[ARRAY_WIDTH], D[ARRAY_WIDTH];
+    Lane C[ARRAY_WIDTH], D[ARRAY_WIDTH];
     Lane B[ARRAY_WIDTH * ARRAY_WIDTH];
     //xor every lane within a column together
     //repeat this for all columns
@@ -49,15 +50,18 @@ void roundFunction (Lane *A)
     {
         C[x] = A[getIndex(x,0)] ^ A[getIndex(x,1)] ^ A[getIndex(x,2)] ^ A[getIndex(x,3)] ^ A[getIndex(x,4)];
     }
-	printf("C Value :%d, %d, %d, %d, %d\n", C[0], C[1],C[2],C[3],C[4]);
+	printf("C Value  After theta :%d, %d, %d, %d, %d\n", C[0], C[1],C[2],C[3],C[4]);
     for (x = 0; x < ARRAY_WIDTH; x++)
     {
-        D[x] = (rotateLeft(C[(x+1) % 5], 1) ^ C[(x+4) % 5]) ;
-	    printf("D Value: %d, %d, %d, %d, %d\n", D[0], D[1],D[2],D[3],D[4]);
-        printf("x:%d RotVal:%llx, VAR:%llx,other:%llx, D[x]: %llx \n ",x, C[(x+1)%5], rotateLeft(C[(x+1)%5],1),C[(x+4) % ARRAY_WIDTH],C[(x+4) % ARRAY_WIDTH] ^ rotateLeft(C[(x+1) % ARRAY_WIDTH], 1));
+        //D[x] = (rotateLeft(C[(x+1) % 5], 1) ^ C[(x+4) % 5]) ;
+        D[x] = (ROL(C[(x+1) % 5], 1) ^ C[(x+4) % 5]) ;
 
 	    //printf("C x-1 %d, Rotate Left Value = %d, value being rotated %d\n ", C[(x+4)%ARRAY_WIDTH], rotateLeft(C[(x+1)%ARRAY_WIDTH], 1), C[(x+1) % ARRAY_WIDTH]);
     }
+    //printf("x:%d RotVal:%d, VAR:%d,other:%d, D[x]: %d \n ",x, C[(x+1)%5], rotateLeft(C[(x+1)%5],1),C[(x+4) % ARRAY_WIDTH],C[(x+4) % ARRAY_WIDTH] ^ rotateLeft(C[(x+1) % ARRAY_WIDTH], 1));
+    printf("values of rotation, %u, %u, %u, %u, %u\n",rotateLeft(C[(1)],1),rotateLeft(C[2],1),rotateLeft(C[3],1),rotateLeft(C[4],1),rotateLeft(C[0],1));
+    printf("values of rotation macro, %d, %d, %d, %d, %d\n",ROL(C[(1)],1),ROL(C[2],1),ROL(C[3],1),ROL(C[4],1),ROL(C[0],1));
+	printf("D Value: %d, %d, %d, %d, %d\n", D[0], D[1],D[2],D[3],D[4]);
 	
     for (x = 0; x < ARRAY_WIDTH; x++)
     {
@@ -67,11 +71,11 @@ void roundFunction (Lane *A)
         }
     }
     printf("after Theta step\n");
-	printf("1st Row: %llx, %llx, %llx, %llx, %llx\n", A[0],A[1],A[2],A[3],A[4]);
-    printf("2st Row: %llx, %llx, %llx, %llx, %llx\n", A[5],A[6],A[7],A[8],A[9]);
-    printf("3st Row: %llx, %llx, %llx, %llx, %llx\n", A[10],A[11],A[12],A[13],A[14]);
-    printf("4st Row: %llx, %llx, %llx, %llx, %llx\n", A[15],A[16],A[17],A[18],A[19]);
-    printf("5st Row: %llx, %llx, %llx, %llx, %llx\n", A[20],A[21],A[22],A[23],A[24]);
+	printf("1st Row: %d, %d, %d, %d, %d\n", A[0],A[1],A[2],A[3],A[4]);
+    printf("2st Row: %d, %d, %d, %d, %d\n", A[5],A[6],A[7],A[8],A[9]);
+    printf("3st Row: %d, %d, %d, %d, %d\n", A[10],A[11],A[12],A[13],A[14]);
+    printf("4st Row: %d, %d, %d, %d, %d\n", A[15],A[16],A[17],A[18],A[19]);
+    printf("5st Row: %d, %d, %d, %d, %d\n", A[20],A[21],A[22],A[23],A[24]);
 
 
     //rho and pi steps
@@ -83,11 +87,11 @@ void roundFunction (Lane *A)
         }
     }
     printf("B after Rho and Pi\n");
-	printf("1st Row: %llx, %llx, %llx, %llx, %llx\n", B[0],B[1],B[2],B[3],B[4]);
-    printf("2st Row: %llx, %llx, %llx, %llx, %llx\n", B[5],B[6],B[7],B[8],B[9]);
-    printf("3st Row: %llx, %llx, %llx, %llx, %llx\n", B[10],B[11],B[12],B[13],B[14]);
-    printf("4st Row: %llx, %llx, %llx, %llx, %llx\n", B[15],B[16],B[17],B[18],B[19]);
-    printf("5st Row: %llx, %llx, %llx, %llx, %llx\n", B[20],B[21],B[22],B[23],B[24]);
+	printf("1st Row: %d, %d, %d, %d, %d\n", B[0],B[1],B[2],B[3],B[4]);
+    printf("2st Row: %d, %d, %d, %d, %d\n", B[5],B[6],B[7],B[8],B[9]);
+    printf("3st Row: %d, %d, %d, %d, %d\n", B[10],B[11],B[12],B[13],B[14]);
+    printf("4st Row: %d, %d, %d, %d, %d\n", B[15],B[16],B[17],B[18],B[19]);
+    printf("5st Row: %d, %d, %d, %d, %d\n", B[20],B[21],B[22],B[23],B[24]);
 
 
         //chi step
@@ -100,11 +104,11 @@ void roundFunction (Lane *A)
         }
     }
 	printf("after Chi step\n");
-	printf("1st Row: %llx, %llx, %llx, %llx, %llx\n", A[0],A[1],A[2],A[3],A[4]);
-    printf("2st Row: %llx, %llx, %llx, %llx, %llx\n", A[5],A[6],A[7],A[8],A[9]);
-    printf("3st Row: %llx, %llx, %llx, %llx, %llx\n", A[10],A[11],A[12],A[13],A[14]);
-    printf("4st Row: %llx, %llx, %llx, %llx, %llx\n", A[15],A[16],A[17],A[18],A[19]);
-    printf("5st Row: %llx, %llx, %llx, %llx, %llx\n", A[20],A[21],A[22],A[23],A[24]);
+	printf("1st Row: %d, %d, %d, %d, %d\n", A[0],A[1],A[2],A[3],A[4]);
+    printf("2st Row: %d, %d, %d, %d, %d\n", A[5],A[6],A[7],A[8],A[9]);
+    printf("3st Row: %d, %d, %d, %d, %d\n", A[10],A[11],A[12],A[13],A[14]);
+    printf("4st Row: %d, %d, %d, %d, %d\n", A[15],A[16],A[17],A[18],A[19]);
+    printf("5st Row: %d, %d, %d, %d, %d\n", A[20],A[21],A[22],A[23],A[24]);
 
 
     return;
@@ -126,8 +130,8 @@ int main() {
                         0, 0, 0, 0, 0,
                         8000000000000000, 0, 0, 0, 0,
                         0, 0, 0, 0, 0};;*/
-    Lane A[ARRAY_WIDTH * ARRAY_WIDTH] = {0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0,
+    Lane A[ARRAY_WIDTH * ARRAY_WIDTH] = {460, 0, 0, 0, 0,
+                        0, 0, 32768, 0, 0,
                         0, 0, 0, 0, 0,
                         0, 0, 0, 0, 0,
                         0, 0, 0, 0, 0};;
@@ -167,11 +171,11 @@ int main() {
         //Iota step
         A[getIndex(0,0)] = A[getIndex(0,0)] ^ RC[round];
         printf("state after completion change:\n");
-        printf("1st Row: %llx, %llx, %llx, %llx, %llx\n", A[0],A[1],A[2],A[3],A[4]);
-        printf("2st Row: %llx, %llx, %llx, %llx, %llx\n", A[5],A[6],A[7],A[8],A[9]);
-        printf("3st Row: %llx, %llx, %llx, %llx, %llx\n", A[10],A[11],A[12],A[13],A[14]);
-        printf("4st Row: %llx, %llx, %llx, %llx, %llx\n", A[15],A[16],A[17],A[18],A[19]);
-        printf("5st Row: %llx, %llx, %llx, %llx, %llx\n", A[20],A[21],A[22],A[23],A[24]);
+        printf("1st Row: %d, %d, %d, %d, %d\n", A[0],A[1],A[2],A[3],A[4]);
+        printf("2st Row: %d, %d, %d, %d, %d\n", A[5],A[6],A[7],A[8],A[9]);
+        printf("3st Row: %d, %d, %d, %d, %d\n", A[10],A[11],A[12],A[13],A[14]);
+        printf("4st Row: %d, %d, %d, %d, %d\n", A[15],A[16],A[17],A[18],A[19]);
+        printf("5st Row: %d, %d, %d, %d, %d\n", A[20],A[21],A[22],A[23],A[24]);
     }
     printf("Done!\n");
     return result;
